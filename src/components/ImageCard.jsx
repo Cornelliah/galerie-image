ca926af3e578f159c6163ea008e3d3394a8ed795
Maxaldo <maxaldo@example.com>
2025-10-28 16:30:51 +0100

    Mis ajour du code galerie image niveau 2

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,27 +1,46 @@
 import React from 'react'
-import Coeur from "../assets/coeur.png"
+import Coeur from "../assets/11.png"
 import { useState } from 'react'
 
 
 function ImageCard({source}) {
   const [count, setCount] = useState(0)
+  const [isZoomed, setIsZoomed] = useState(false)
   
-  const handleClick = () => {
+  const handleLike = (e) => {
+    e.stopPropagation()
     setCount(count + 1)
   }
   
+  const handleImageClick = () => {
+    setIsZoomed(true)
+  }
+  
+  const closeZoom = () => {
+    setIsZoomed(false)
+  }
+  
   return (
-     
+     <>
     <div className='card'>               
-         <img className='Hover' src={source} alt="" onClick={handleClick}/>   
+         <img className='Hover' src={source} alt="" onClick={handleImageClick}/>   
 
-         <div className='icone' onClick={handleClick}>
+         <div className='icone' onClick={handleLike}>
               <img src={Coeur} alt="like" />   
               <span className='count'>{count}</span>
          </div>
           
      </div>
      
+     {isZoomed && (
+       <div className='modal' onClick={closeZoom}>
+         <div className='modal-content'>
+           <span className='close'>&times;</span>
+           <img src={source} alt="" className='zoomed-image'/>
+         </div>
+       </div>
+     )}
+     </>
   )
 }
 
